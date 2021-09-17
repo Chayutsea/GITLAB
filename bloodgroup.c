5037eb217e02f81712d4bf3f6d06297523e1af7d
xxxxx <chayut_ja@outlook.co.th>
2021-09-17 07:45:23 +0700

    change a

--- bloodgroup.c
+++ bloodgroup.c
@@ -13,8 +13,8 @@ void main()
       printf("What is your blood group [A, B, AB, or O]? ");
       scanf("%s", G);
 
-      if (strcmp(G, "A") !=0  &&
-                            strcmp(G, "B") !=0  &&
+      if (strcmp(G, "Z") !=0  &&
+                            strcmp(G, "Y") !=0  &&
                             strcmp(G, "AB") !=0   &&
                             strcmp(G, "O") !=0 )
         printf("Blood group %s is incorrect! Please try again.\n", G);
